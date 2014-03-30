91b4199b563840b44741de9634603be435aa5711
Marcus Bristol <marcus.bristol@rededge.co.nz>
2014-03-30 15:32:17 +1300

    Added handlers for noty

--- app/js/app.js
+++ app/js/app.js
@@ -23,12 +23,54 @@ meetupRaffler.config(['$routeProvider',
 				templateUrl: 'partials/home.html',
 				controller: 'home'
 			})
+			.when('/login', {
+				templateUrl: 'partials/login.html',
+				controller: 'authorisation'
+			})
 			.when('/meetups', {
 				templateUrl: 'partials/meetup-list.html',
 				controller: 'meetupList'
 			})
-			.otherwise({
-				redirectTo: '/'
+			.when('/meetups/:groupId', {
+				templateUrl: 'partials/meetup-event.html',
+				controller: 'meetupEvent'
+			//})
+			//.otherwise({
+			//	redirectTo: '/'
 			});
+	}
+]);
+
+// this is run after angular is instantiated and bootstrapped
+meetupRaffler.run(['$rootScope', '$location', 'authService',
+	function($rootScope, $location, authService) {
+
+		// *****
+		// Initialize authentication
+		// *****
+		$rootScope.authService = authService;
+
+		$rootScope.$watch('authService.authorized()', function () {
+
+			// if never logged in, do nothing (otherwise bookmarks fail)
+			if ($rootScope.authService.initialState()) {
+				// we are public browsing
+				return;
+			}
+
+			// instantiate and initialize an auth notification manager
+			$rootScope.authNotifier = new NotificationManager($rootScope);
+
+			// when user logs in, redirect to home
+			if ($rootScope.authService.authorized()) {
+				$rootScope.authNotifier.notify('information', 'Successfully logged in!');
+			}
+
+			// when user logs out, redirect to home
+			if (!$rootScope.authService.authorized()) {
+				$rootScope.authNotifier.notify('information', 'Thanks for visiting. You have been signed out.');
+			}
+
+		}, true);
 	}
 ]);
