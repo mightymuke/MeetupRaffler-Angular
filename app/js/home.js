3eaf5a60f64ad954b8ad6b3cbc666c49a6220d9c
Marcus Bristol <marcus.bristol@rededge.co.nz>
2014-04-06 16:41:03 +1200

    Added 2 way binding demo

--- app/js/home.js
+++ app/js/home.js
@@ -4,6 +4,18 @@
 
 meetupRafflerControllers.controller('home', ['$scope',
 	function($scope) {
+		$scope.edit = function() {
+			console.log("edit");
+			$scope.isEditing = true;
+		};
+
+		$scope.doneEditing = function() {
+			console.log("doneEditing");
+			$scope.isEditing = false;
+		};
+
+		$scope.isEditing = false;
+		$scope.title = "Dashboard";
 	}]
 );
 
