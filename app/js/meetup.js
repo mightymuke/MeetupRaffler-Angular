3cd749df22aabec4544952462825ef9e9229bebb
Marcus Bristol <mightymuke@gmail.com>
2014-08-12 07:13:04 +1200

    First part of the shuffle animation done
    This will collect the cards and do a quick shuffling effect. Next part
    is to slowly flip the cards until the correct card is found

--- app/js/meetup.js
+++ app/js/meetup.js
@@ -9,32 +9,93 @@ define([
 	meetupRafflerControllers.controller('meetup', ['$scope', '$rootScope', '$route', 'meetupEventService', 'meetupEventRsvpsService', 'authService',
 		function($scope, $rootScope, $route, meetupEventService, meetupEventRsvpsService, authService) {
 			$scope.getRandomMember = function() {
-				var min = 0;
-				var max = $scope.rsvps.length - 1;
-				var selectedPrize = $("#prize option:selected");
-				var selectedPrizeText = selectedPrize.text().trim();
-				var selectedPrizeValue = selectedPrize.val();
-				if (selectedPrizeText !== "") {
+				// var min = 0;
+				// var max = $scope.rsvps.length - 1;
+				// var selectedPrize = $("#prize option:selected");
+				// var selectedPrizeText = selectedPrize.text().trim();
+				// var selectedPrizeValue = selectedPrize.val();
+				// if (selectedPrizeText !== "") {
 
-					var memberFound = false;
-					var count = 0;
-					while (!memberFound && count < 5) {
-						var memberIndex = Math.floor(Math.random() * (max - min + 1)) + min;
-						var member = $scope.rsvps[memberIndex].member;
-						memberFound = memberIsValidForPrize(member.member_id, selectedPrizeValue);
-						count += 1;
-					}
+				// 	var memberFound = false;
+				// 	var count = 0;
+				// 	while (!memberFound && count < 5) {
+				// 		var memberIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+				// 		var member = $scope.rsvps[memberIndex].member;
+				// 		memberFound = memberIsValidForPrize(member.member_id, selectedPrizeValue);
+				// 		count += 1;
+				// 	}
 
-					if (count < 5) {
-						$rootScope.authNotifier.notify('success', 'Congratulations<br/>' + member.name + ' has won a ' + selectedPrizeText + '!');
-					} else {
-						$rootScope.authNotifier.notify('error', 'Unable to find a winner');
-					}
+				// 	if (count < 5) {
+				// 		$rootScope.authNotifier.notify('success', 'Congratulations<br/>' + member.name + ' has won a ' + selectedPrizeText + '!');
+				// 	} else {
+				// 		$rootScope.authNotifier.notify('error', 'Unable to find a winner');
+				// 	}
 
-				} else {
-					$rootScope.authNotifier.notify('error', 'Please select a prize you silly billy!');
-				}
+				// } else {
+				// 	$rootScope.authNotifier.notify('error', 'Please select a prize you silly billy!');
+				// }
+				var $memberListings = $('.member-listing');
+				var $parent = $memberListings.parent();
+				var memberCount = $memberListings.length;
+				var i = 0;
+
+				// Fix their positions
+				$memberListings.each(function(index) {
+					var $this = $(this);
+					$this.css("left", Math.max(0, $this.position().left) + "px");
+					$this.css("top", Math.max(0, $this.position().top) + "px");
+					$this.css("z-index", index * -1);
+				})
+				$memberListings.css("position", "absolute");
+
+				// Do a quick shuffle
+				console.log($memberListings);
+				var $firstListing = $($memberListings[0]);
+				var thisLeft = Math.max(0, (($parent.outerWidth() - $firstListing.outerWidth()) / 2) + $parent.position().left + $(window).scrollLeft());
+				var thisTop = Math.max(0, (($(window).height() - $firstListing.outerHeight()) / 2) + $(window).scrollTop());
+				$memberListings.each(function(index) {
+					$(this)
+						.animate(
+						{
+							'left' : thisLeft + "px",
+							'top' : thisTop + "px"
+						},
+						{
+							duration: 1000,
+							easing: 'linear',
+							done: function() {
+								if (index === 0) {
+									chain($memberListings, 0, -100);
+								}
+							}
+						});
+					});
+
 			}
+
+			var chain = function(toAnimate, ix, zindex){
+				zindex--;
+				console.log("item " + ix + "; zindex: " + zindex);
+				console.log(toAnimate[ix]);
+				if(toAnimate[ix]){
+					var moveLeft = 120;
+					var moveTop = Math.floor(Math.random() * (120 + 50) - 50); //50
+					var thisLeft = $(toAnimate[ix]).position().left;
+					var thisTop = $(toAnimate[ix]).position().top;
+					$(toAnimate[ix])
+						.animate(
+							{
+								'z-index': zindex,
+							},
+							{
+								duration: 10,
+								easing: 'linear',
+								done: function() {
+									chain(toAnimate, ix + 1, zindex);
+								}
+							});
+				}
+			};
 
 			var memberIsValidForPrize = function(member_id, prize) {
 				console.log('checking ' + member_id + " for " + prize);
